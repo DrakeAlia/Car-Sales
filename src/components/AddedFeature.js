19734fdd946706e8656e75d1f9e944b39a17c84f
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-05-13 14:58:16 -0700

    add max height

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -21,11 +21,10 @@ const AddedFeature = props => {
 // mapStateToProps takes state data for features so its properties can be accessed via props in this component
 const mapStateToProps = state => {
     return {
-      // the state data for all the feature object's properties can be accessed via props
+      
         features: state.features 
     }
 }
 
 // the connect function links the AddedFeature component with the store for retrieving data it needs from the store
-// connect dispatches the removeFeature action action creator to the reducer and allows removeFeature to be accessed via props
 export default connect(mapStateToProps, { removeFeature }) (AddedFeature);
